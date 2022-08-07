e30b901db8f3ef1fc84ff1bd9e5cdf5ca5fae835
Hector Castro <hectcastro@gmail.com>
2022-08-07 14:00:04 +0000

    fix: command flags not resetting between tests

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -8,13 +8,29 @@ import (
 	"time"
 
 	"github.com/nbio/st"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
-func execute(args string) string {
+func ResetSubCommandFlagValues(t *testing.T, root *cobra.Command) {
+	t.Helper()
+
+	root.Flags().VisitAll(func(f *pflag.Flag) {
+		if f.Changed {
+			f.Value.Set(f.DefValue)
+			f.Changed = false
+		}
+	})
+}
+
+func execute(t *testing.T, args string) string {
+	t.Helper()
+
 	actual := new(bytes.Buffer)
 
 	RootCmd.SetOut(actual)
 	RootCmd.SetErr(actual)
+	ResetSubCommandFlagValues(t, RootCmd)
 	RootCmd.SetArgs(strings.Split(args, " "))
 	RootCmd.Execute()
 
@@ -22,28 +38,28 @@ func execute(args string) string {
 }
 
 func Test_RootCmd_NoArgs(t *testing.T) {
-	actual := execute("")
+	actual := execute(t, "")
 	expected := "Error: required flag(s) \"owner\", \"repo\" not set"
 
 	st.Assert(t, strings.Contains(actual, expected), true)
 }
 
 func Test_RootCmd_Version(t *testing.T) {
-	actual := execute("--version")
+	actual := execute(t, "--version")
 	expected := fmt.Sprintf("gh-metrics version %s", Version)
 
 	st.Assert(t, strings.Contains(actual, expected), true)
 }
 
 func Test_RootCmd_OnlyOwner(t *testing.T) {
-	actual := execute("--owner=cli")
+	actual := execute(t, "--owner=cli")
 	expected := "Error: required flag(s) \"repo\" not set"
 
 	st.Assert(t, strings.Contains(actual, expected), true)
 }
 
 func Test_RootCmd_OnlyRepo(t *testing.T) {
-	actual := execute("--repo=cli")
+	actual := execute(t, "--repo=cli")
 	expected := "Error: required flag(s) \"owner\" not set"
 
 	st.Assert(t, strings.Contains(actual, expected), true)
